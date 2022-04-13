ad399f339f7fd291ea0328e9519085e313594048
ChristoT98 <christolakshan@gmail.com>
2022-04-13 19:24:00 +0530

    Navbar created

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav id="nav">
+    <router-link :class="{active: $route.name === 'Home'}" to="/">Home</router-link> |
+    <router-link :class="{active: $route.name === 'Cart'}" to="/cart">Cart</router-link>
   </nav>
   <router-view/>
 </template>
@@ -12,19 +12,43 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+html, body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  overflow-x: hidden;
+}
+
+body {
+  background-color: rgb(245, 245, 245)
+}
+
+#nav {
+  padding: 20px;
+  width: 100%;
+  height: 50px;
+  background-color: #04182c;
+  line-height: 30px;
 
   a {
+    color: white;
     font-weight: bold;
-    color: #2c3e50;
+    text-decoration: none;
+    margin: 0 5px 0 5px;
+    font-size: 1.25rem;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+    &.active {
+    color: #04182c;
+    background-color: white;
+    padding: 6px;
+    border-radius: 5px;
   }
+  }
+}
+
+.text-center {
+  text-align: center;
 }
 </style>
